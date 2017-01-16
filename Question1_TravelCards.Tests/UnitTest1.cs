0de33c9a00899321214ff985b7cf5be5a4add8e1
Alexander Saraev <Alksar@yandex.ru>
2017-01-16 13:07:57 +0300

    project init

---
+++ Question1_TravelCards.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace Question1_TravelCards.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
