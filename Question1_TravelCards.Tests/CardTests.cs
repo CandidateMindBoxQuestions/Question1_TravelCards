87ae422360a54261e0ed1794f52d528c0b0f236c
Alexander Saraev <Alksar@yandex.ru>
2017-01-16 13:53:02 +0300

    add card class

---
+++ Question1_TravelCards.Tests/CardTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace Question1_TravelCards.Tests
+{
+    [TestFixture]
+    public class CardTests
+    {
+        private Card _card;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            _card = new Card("from", "to");
+        }
+
+        [Test]
+        public void Constructor_works_correctly()
+        {
+            Assert.That((_card.From == "from") && (_card.To == "to"));
+        }
+    }
+}
---
+++ Question1_TravelCards/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1_TravelCards
+{
+    public class Card
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public Card(string from, string to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}
